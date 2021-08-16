cbd57fcd132b1bb956cbc4b62eb21a16b969d76f
selcouthonism <37469324+selcouthonism@users.noreply.github.com>
2021-08-16 10:19:34 +0300

    1_ArithmeticOperators added

---
+++ notes/1_ArithmeticOperators.py
@@ -0,0 +1,31 @@
+###  Arithmetic Operators  ###
+
+#print() method writes to console.
+#Semicolon(;) does not used in python.
+#It is used in other programming languages like java, C, C++, C#  after method calls
+#Python: print(5+3) 
+#java: System.out.println(5+3);
+
+#Addition 5+3 = 8
+print(5+3)
+
+#Subtraction 5-3 = 2
+print(5-3)
+
+#Multiplication 5*3 = 15
+print(5*3)
+
+#Division 10/5 = 2, 14/5 = 2.8
+print(10/5)
+print(14/5)
+
+#Modulus (the remainder) 5%3 = 2, 13%7 = 6
+print(5%3)
+
+#Exponentiation(power) 2**3 = 8 -> 2*2*2
+print(2**3)
+
+#Floor division 14//5 = 2
+#Normal division result ise 14/5 = 2.8 
+#but floor division takes the decimal numbers before .  14//5 = 2 
+print(14//5)
