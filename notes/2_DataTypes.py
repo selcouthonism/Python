07b1ee46330f2bc2074318d477807a4eceb7e7fc
selcouthonism <37469324+selcouthonism@users.noreply.github.com>
2021-08-16 15:25:05 +0300

    2_DataTypes.py created
    Only explanations added

---
+++ notes/2_DataTypes.py
@@ -0,0 +1,20 @@
+'''
+In other languages (java, C, C#), we define the data type and variable name. 
+After that we cannot change the data type.
+In java:
+int number = 10;
+float floatNumber = 10.5;
+String text = "Hello World"; 
+List<Integer> list = new ArrayList<>();
+
+number = "Hello World"; -> Compiler error
+
+Python has dynamic variable assignment.
+We can assign an int,float, String... value to a variable 
+and then can change its type by assigning new value with new data type.
+In python:
+number = 10  -> python defines number variable as int type
+number = 10.8  -> python updates number variable as float type
+number = "Hello World"  -> python updates number variable as String type
+number = set() -> updates as empty set
+'''
